54c78acd4e03bda3757448115109c1f4127e66d5
shz2002 <sj1050437739@gmail.com>
2023-04-04 00:13:59 +0800

    1.创建文件夹对文件进行整理 2.创建OpenGL基础代码框架 3.导入必要的库(glm,stb_image,imgui...)

---
+++ OpenGLLight/src/main.cpp
@@ -0,0 +1,67 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+// Function Declaration
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void process_input(GLFWwindow* window);
+
+float screen_Width = 800.0f;
+float screen_Height = 600.0f;
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(screen_Width, screen_Height, "OpenGL_Light", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Window Event Callback
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to init GLAD" << std::endl;
+
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		process_input(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Renderer Order
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width,int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
