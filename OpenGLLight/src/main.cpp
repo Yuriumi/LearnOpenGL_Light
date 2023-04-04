ff6d64bee39aa4792b964d18683d4af3f66e24fa
shz2002 <sj1050437739@gmail.com>
2023-04-04 16:02:22 +0800

    初始化ImGui,创建默认窗口

--- OpenGLLight/src/main.cpp
+++ OpenGLLight/src/main.cpp
@@ -5,17 +5,30 @@
 
 #include <glm/gtc/type_ptr.hpp>
 
+#include "../ref/imgui/imgui.h"
+#include "../ref/imgui/imgui_impl_glfw.h"
+#include "../ref/imgui/imgui_impl_opengl3.h"
+
 #include "../ref/Camera.h"
 #include "../ref/Shader.h"
 
 // Function Declaration
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void process_input(GLFWwindow* window);
+ImGuiIO& init_imgui(GLFWwindow* window);
+void update_gui_frame();
+void draw_imgui_window(const char* title, ImGuiIO& io);
+void render_gui();
+void destory_gui_data();
 
 float screen_Width = 800.0f;
 float screen_Height = 600.0f;
 
 Camera mainCamera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+float currentTime = 0.0f;
+float deltaTime = 0.0f;
+float lastTime = 0.0f;
 
 int main()
 {
@@ -110,12 +123,20 @@ int main()
 	glm::mat4 view;
 	glm::mat4 projection;
 
+	ImGuiIO& io = init_imgui(window);
+
 	while (!glfwWindowShouldClose(window))
 	{
+		currentTime = (float)glfwGetTime();
+		deltaTime = currentTime - lastTime;
+		lastTime = currentTime;
+
 		process_input(window);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		update_gui_frame();
 
 		// Renderer Order
 		basicLight.use();
@@ -136,10 +157,14 @@ int main()
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 		glBindVertexArray(0);
 
+		draw_imgui_window("OpenGL_Light_Settings", io);
+		render_gui();
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	destory_gui_data();
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
@@ -154,4 +179,47 @@ void process_input(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+}
+
+ImGuiIO& init_imgui(GLFWwindow* window)
+{
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplGlfw_InitForOpenGL(window,true);
+	ImGui_ImplOpenGL3_Init("#version 330 core");
+
+	return io;
+}
+
+void update_gui_frame()
+{
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	ImGui::NewFrame();
+}
+
+void draw_imgui_window(const char* title, ImGuiIO& io)
+{
+	ImGui::Begin(title);
+
+	// Draw window
+
+	ImGui::End();
+}
+
+void render_gui()
+{
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void destory_gui_data()
+{
+	ImGui_ImplGlfw_Shutdown();
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui::DestroyContext();
 }
